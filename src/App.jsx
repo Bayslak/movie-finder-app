41cbd7a849833fffa6b2f8b3b945ae087e7b56c4
Alessandro Esposito <ale.esposito150495@gmail.com>
2025-12-29 12:23:34 +0100

    feat: first hook and state

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import './App.css'
+import {useState} from "react";
 
 function Card({ title } ) {
+    const [hasLiked, setHasLiked] = useState(false);
+
     return (
         <div className="card">
             <h2>{title}</h2>
+
+            <button onClick={() => setHasLiked(!hasLiked)}>
+                { hasLiked ? '❤️' : '🤍' }
+            </button>
         </div>
     );
 }
 
 function App() {
+
     return(
         <div className="card-container">
             <Card title="Star Wars"/>
