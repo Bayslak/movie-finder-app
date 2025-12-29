a043f1c913d5fe66ec8f86e8a2428865858fbe6e
Alessandro Esposito <ale.esposito150495@gmail.com>
2025-12-29 13:04:05 +0100

    feat: init tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,13 @@
 import './App.css'
-import {useEffect, useState} from "react";
-
-function Card({ title } ) {
-    const [count, setCount] = useState(0)
-    const [hasLiked, setHasLiked] = useState(false);
-
-    useEffect(() => {
-        console.log(`${title} has been liked: ${hasLiked}`);
-    }, [hasLiked]) //Array with values means that React will try to see if the value changes and then fire the useEffect
-
-    useEffect(() => {
-        console.log(`${title} card has been rendered`);
-    }, []) //Empty array means that it runs only once when loading the component
-
-    function likeAFilm() {
-        setHasLiked((prevState) => !prevState);
-        setCount(count + 1);
-    }
-
-    return (
-        <div className="card">
-            <h2>{title} <br/> {count || null}</h2>
-
-            <button onClick={likeAFilm}>
-                { hasLiked ? '❤️' : '🤍' }
-            </button>
-        </div>
-    );
-}
 
 function App() {
-
-    return(
-        <div className="card-container">
-            <Card title="Star Wars"/>
-            <Card title="Lord of the Rings"/>
-            <Card title="Wake Up The Dead Man"/>
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
         </div>
-    );
+    )
 }
 
 export default App
