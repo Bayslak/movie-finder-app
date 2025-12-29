cf2edc5860c967dc700ad9d2bd26670cc4b7be0c
Alessandro Esposito <ale.esposito150495@gmail.com>
2025-12-29 13:31:48 +0100

    feat: search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-import './App.css'
+import Search from "./components/Search.jsx";
+import {useState} from "react";
 
 function App() {
+    const [searchTerm, setSearchTerm] = useState('')
+
     return (
         <main>
             <div className="pattern"/>
@@ -11,7 +14,7 @@ function App() {
                     <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
                 </header>
 
-                <p>Search</p>
+                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
             </div>
         </main>
     )
---
+++ src/components/Search.jsx
@@ -0,0 +1,19 @@
+﻿import React from 'react'
+
+const Search = ({searchTerm, setSearchTerm}) => {
+    return (
+        <div className="search">
+            <div>
+                <img src="../../public/search.svg" alt="Search" />
+                <input
+                    type="text"
+                    placeholder="Search throught thousands of movies"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Search;
