f1538688ca69791f33ce60dc3cbff31999d568a7
Alessandro Esposito <ale.esposito150495@gmail.com>
2025-12-29 12:43:50 +0100

    feat: useEffect and custom function for card

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import './App.css'
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 function Card({ title } ) {
+    const [count, setCount] = useState(0)
     const [hasLiked, setHasLiked] = useState(false);
+
+    useEffect(() => {
+        console.log(`${title} has been liked: ${hasLiked}`);
+    }, [hasLiked]) //Array with values means that React will try to see if the value changes and then fire the useEffect
+
+    useEffect(() => {
+        console.log(`${title} card has been rendered`);
+    }, []) //Empty array means that it runs only once when loading the component
+
+    function likeAFilm() {
+        setHasLiked((prevState) => !prevState);
+        setCount(count + 1);
+    }
 
     return (
         <div className="card">
-            <h2>{title}</h2>
+            <h2>{title} <br/> {count || null}</h2>
 
-            <button onClick={() => setHasLiked(!hasLiked)}>
+            <button onClick={likeAFilm}>
                 { hasLiked ? '❤️' : '🤍' }
             </button>
         </div>
